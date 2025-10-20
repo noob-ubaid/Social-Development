02373a3d2ff7d1f6f67d8db517f297256b02449f
noob-ubaid <ubaidurrahman661@gmail.com>
2025-10-20 23:43:52 +0600

    solved the navbar section

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -78,39 +78,8 @@ const Navbar = () => {
   });
 
   return (
-<<<<<<< HEAD
-    <div className="sticky top-0 bg-main/10  backdrop-blur-2xl rounded-none md:rounded-md z-50  py-4 px-3 md:px-4  overflow-visible">
+    <div className="sticky top-0 bg-main/10 backdrop-blur-2xl rounded-none md:rounded-md z-50 py-4 px-3 md:px-4 overflow-visible">
       <div className="max-w-[1600px] mx-auto flex items-center justify-between">
-        <div className="flex items-center ">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4 dark:text-white"
-              fill="none"
-              viewBox="0 0 20 20"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dark:bg-gray-800 dropdown-content bg-base-100 rounded-box z-50 mt-3 w-52 p-2 shadow"
-          >
-            {navLinks.map((link) => (
-              <NavLink
-                key={link.name}
-                to={link.path}
-                className="text-lg text-center nav px-4 py-2 rounded-md dark:text-white font-medium"
-=======
-    <div className="sticky top-0  bg-main/10 backdrop-blur-2xl rounded-none md:rounded-md z-50  py-4 px-3 md:px-4 overflow-visible">
-      <div className="flex items-center justify-between max-w-[1600px] mx-auto">
         <div className="flex items-center">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -120,7 +89,6 @@ const Navbar = () => {
                 fill="none"
                 viewBox="0 0 20 20"
                 stroke="currentColor"
->>>>>>> ubaidur-rahman
               >
                 <path
                   strokeLinecap="round"
@@ -224,7 +192,6 @@ const Navbar = () => {
             )}
           </button>
         </div>
-      </div>
       </div>
     </div>
   );
